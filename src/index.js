0d79c9715a24d54050f53794e4ec440f1a45fe24
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-08-22 16:06:00 +0530

    Update index.js to wrap "Hello World" and welcome message in a React Fragment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello World</h1>,
+    <React.Fragment>
+        <h1>Hello World</h1>
+        <h2>Welcome to coding world</h2>
+  </React.Fragment>
   
 );

0a3fcf100222a3011f6d94e3800953e1c5b87da8
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-08-22 12:55:11 +0530

    Refactor code structure for improved readability and maintainability

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Hello World</h1>,
+  
 );
